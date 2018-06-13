b5c41ffbd405fc4f352478ada1588c3d9de618c4
tanya-agarwal06 <tanya6121998@gmail.com>
2018-06-14 00:19:13 +0530

    commands used

---
+++ assignment -2.py
@@ -0,0 +1,25 @@
+#1
+'''print("welcome to acadview")'''
+
+#2
+'''print("acad" + "view")'''
+
+#3
+'''a=1
+b=6
+c=98
+print(a,b,c)'''
+
+#4
+'''print("let\'s get started on screen")'''
+
+#5
+'''s="acadview"
+course="python"
+fees=5000
+print("welcome to %s,course is %s and fees is %d" % (s,course,fees))'''
+
+#6
+'''name="tony stark"
+salary= 1000000
+print('%s %d' %(name,salary))'''
